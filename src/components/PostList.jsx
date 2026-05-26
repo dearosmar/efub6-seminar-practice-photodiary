2c3f7c27a431f702c1102afb1a85cd7988737e3c
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-05-26 19:29:52 +0900

    feat: 코드 수정

---
+++ src/components/PostList.jsx
@@ -0,0 +1,12 @@
+function PostList({ posts }) {
+  // TODO 1️⃣: posts가 비어있을 때 "아직 작성된 일기가 없어요" 메시지 표시
+
+  return (
+    <div className="post-list">
+      {/* TODO 2️⃣: posts 배열을 map으로 순회하며 각 일기 렌더링 */}
+      {/* 힌트: key, img src, title 표시 */}
+    </div>
+  );
+}
+
+export default PostList;
